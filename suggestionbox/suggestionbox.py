bd3c4e6f75a485e1f5c4acffa2e6565c896c0346
Jason Wright <jason.wright.2007@gmail.com>
2017-07-22 12:52:54 -0500

    shotgun blast of attempts

--- suggestionbox/suggestionbox.py
+++ suggestionbox/suggestionbox.py
@@ -165,8 +165,11 @@ class SuggestionBox:
             where = server.get_channel(output)
             if where is not None:
                 message = await self.bot.send_message(where, embed=em)
-                await self.bot.add_reaction(message, '\N{UPWARDS ARROW}')#':up_arrow:')#'\ud83d\udc4d')#'⬆️')
-                await self.bot.add_reaction(message, '\N{DOWNWARDS ARROW}')#':down_arrow')#'\ud83d\udc4e')#'⬇️')
+                await self.bot.add_reaction(message, '\N{THUMBS UP SIGN}')
+                await self.bot.add_reaction(message, '↑')
+                await self.bot.add_reaction(message, '\N{UPWARDS ARROW SIGN}')
+                await self.bot.add_reaction(message, '\N{UP ARROW SIGN}')
+                await self.bot.add_reaction(message, '↓')
                 # await self.bot.add_reaction(message,'❌')
         try:
             self.settings[server.id]['usercache'].remove(author.id)
