6d3e214456d8e326bcbdde21aebf8db5334bba76
JJW <jason.wright.2007@gmail.com>
2017-07-24 17:03:36 -0500

    unicode emoji

--- suggestionbox/suggestionbox.py
+++ suggestionbox/suggestionbox.py
@@ -165,8 +165,8 @@ class SuggestionBox:
             where = server.get_channel(output)
             if where is not None:
                 message = await self.bot.send_message(where, embed=em)
-                await self.bot.add_reaction(message, ':up_arrow:')#'\ud83d\udc4d')#'⬆️')
-                await self.bot.add_reaction(message, ':down_arrow')#'\ud83d\udc4e')#'⬇️')
+                await self.bot.add_reaction(message, '\N{UPWARDS BLACK ARROW}')#'\ud83d\udc4d')#'⬆️')
+                await self.bot.add_reaction(message, '\N{DOWNWARDS BLACK ARROW}')#'\ud83d\udc4e')#'⬇️')
                 # await self.bot.add_reaction(message,'❌')
         try:
             self.settings[server.id]['usercache'].remove(author.id)
